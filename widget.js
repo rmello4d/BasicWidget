48fb74ae4b6b1f0c0158cfaa2ce1c0496561507e
Ricardo <ricardo.mello@4d.com>
2013-10-17 20:00:48 +0200

    first commit

---
+++ widget.js
@@ -0,0 +1,14 @@
+(function() {
+
+	//Defining the base architecture based on the waf-core/widget class
+	var widget = WAF.require('waf-core/widget');
+
+	// Creating the Basic Container widget 
+	var BasicWidget = widget.create('BasicWidget');
+    
+	// Creating a basic widget with an SPAN tag instead of the DIV
+	BasicWidget.tagname = 'span';
+
+})();
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html
